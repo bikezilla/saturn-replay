9e6b5262098c11981adc18f73f587752eced2eb1
Vladimir Konushliev <v.konushliev@gmail.com>
2022-09-19 17:07:10 +0300

    Configure everything

--- saturn-replay.js
+++ saturn-replay.js
@@ -2,33 +2,44 @@ const puppeteer = require('puppeteer');
 const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
 
 (async () => {
-  const browser = await puppeteer.launch('/Applications/Firefox.app/');
+  const headless = true
+  const production = true
 
+  const eventId = 16
+  const start = new Date("2022-09-10 13:30")
+  const duration = 60 // minutes
+  const interval = 5   // minutes
+  const width = 1920
+  const height = 1200
+
+  const base = production ? 'http://live.kriva.org' : 'http://localhost:5000'
+  const puppeteerConfig = headless ? '/Applications/Firefox' : {product: 'chrome', headless: false}
+
+  const browser = await puppeteer.launch(puppeteerConfig)
   const page = await browser.newPage();
-  await page.setViewport({width: 1920, height: 1200})
+  await page.setViewport({width: width, height: height})
 
-  const config = {
+  const recorderConfig = {
     followNewTab: true,
-    fps: 1,    
+    fps: 1,
     aspectRatio: '8:5'
   }
-  const recorder = new PuppeteerScreenRecorder(page, config);
-  await recorder.start(`24@${config.fps}fps.mp4`);
+  const recorder = new PuppeteerScreenRecorder(page, recorderConfig);
+  await recorder.start(`24@${recorderConfig.fps}fps.mp4`);
 
-  console.log('start')
-  const interval = 1   // minutes
-    const start = new Date("2021-09-11 12:59")    
+  console.log('Start')
 
-    for(var offset = 0; offset <= 1441; offset = offset + interval) {
-      const atTime = new Date(start.getTime() + offset*60000).toUTCString()
+  for(var offset = 0; offset <= duration; offset = offset + interval) {
+    const atTime = new Date(start.getTime() + offset*60000).toUTCString()
+    const url = `${base}/live/marathon_dashboards/${eventId}?at_time=${atTime}`
 
-      await page.goto(`http://localhost:5000/live/marathon_dashboards/12?at_time=${atTime}`)          
-      await page.waitForTimeout(300)
-      await page.waitForSelector('footer')      
-    
-    } 
+    console.log(`Requesting ${url}`)
+    await page.goto(url)
+    await page.waitForTimeout(300)
+    await page.waitForSelector('footer')
+  }
 
-  console.log('end')
+  console.log('End')
   await recorder.stop();
   await browser.close()
 })();
